a2671fc1bc53cb082d7561e96e72468c98ca56cf
Schwimmer, Andrew <Andrew.Schwimmer@mlb.com>
2020-05-12 20:35:29 -0400

    added NeoControllerTest and NearEarthObjectViewTest

--- src/main/java/schwimmer/nasa/neo/NeoService.java
+++ src/main/java/schwimmer/nasa/neo/NeoService.java
@@ -1,9 +1,7 @@
 package schwimmer.nasa.neo;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
+import retrofit2.http.*;
 
 public interface NeoService {
 
